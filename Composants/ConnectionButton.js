1b6f0f1c75df99e0f9a1742960145e0746e8a276
bintumustafa <65418892+bintumustafa@users.noreply.github.com>
2022-04-05 12:16:25 +0000

    Create ConnectionButton.js

---
+++ Composants/ConnectionButton.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import {Text, TouchableOpacity} from 'react-native';
+export default function ConnectionButton ({title, color}){ 
+    return(
+        <TouchableOpacity style={{backgroundColor:{color}, width:'40%', padding:20, borderRadius:30, alignItems:'center', flexDirection:'row'}}>
+               <Text style={{fontSize:20, color:'black'}}> {title}</Text>
+        </TouchableOpacity>
+    )
+}

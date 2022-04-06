102517cfa67e806617225cfca71eca498a9748c5
bintumustafa <bijousow74@gmail.com>
2022-04-06 06:20:25 +0000

    Modification de App et ajout de la vue d'accueil

---
+++ Index.tsx
@@ -0,0 +1,44 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import ConnectionButton from './components/ConnectionButton';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={{
+          width:150, height:150,
+          marginTop:"15%",
+        }}
+        source={
+          require("./assets/imgMgl.jpeg")
+        }
+      />
+      <Text 
+        style={{
+          fontSize: 38,
+          color: "rgba(255, 255, 255, 1)",
+          fontWeight: "bold",
+        }}>
+          Discutez instantanément simplement!
+      </Text>
+      <View style={{flexDirection:"row", top:"55%"}}>
+        <ConnectionButton title="Connexion" color={'white'}/>
+        <ConnectionButton title ="Inscription" color={'#5D96F5'}/>      
+        <StatusBar style="auto" />
+      </View>
+      </View>
+  );
+}
+
+//Styled components
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#5D96F5',
+    position : 'relative',
+    alignItems: 'center', 
+  },
+  
+  
+});
